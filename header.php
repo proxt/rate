16591abe08a9ea292c6dcc5f61978f06fcf8ce30
proxt <skyrimxts@gmail.com>
2017-06-26 14:58:24 +0700

    перенес на php

---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> 
+	<link rel="stylesheet" type="text/css" href="style/styles.css">
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+	<script type="text/javascript" src="js/search.js"></script>
+	<title>rate</title>
+</head>
+<body>
+<div id="header">
+	
+
+</div>
+
+
+</div>
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+	include "header.php";
+	include "left.php";
+	include "right.php";
+	include "footer.php";
+?>
+</body>
+</html>
